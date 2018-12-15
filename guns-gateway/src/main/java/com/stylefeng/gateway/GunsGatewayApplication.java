7df33c07a6796b33a1cfcb0d059f0a7a0d495dcb
liupantao <804878782@qq.com>
2018-12-15 21:05:30 +0800

    增加模块 apigateway

---
+++ guns-gateway/src/main/java/com/stylefeng/gateway/GunsGatewayApplication.java
@@ -0,0 +1,14 @@
+package com.stylefeng.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GunsGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GunsGatewayApplication.class, args);
+    }
+
+}
+
